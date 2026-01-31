72e00807dafc768244d94be8075fceca33c57251
Migration Agent <migration@schemajeli.dev>
2026-01-31 15:11:24 -0500

    fix: Add random suffix to resource group for unique names per deployment

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -44,9 +44,16 @@ provider "azurerm" {
 
 data "azurerm_client_config" "current" {}
 
+# Random suffix for resource group (ensures unique name per deployment)
+resource "random_string" "rg_suffix" {
+  length  = 6
+  special = false
+  upper   = false
+}
+
 # Resource Group
 resource "azurerm_resource_group" "main" {
-  name     = "${var.project_name}-${var.environment}-rg"
+  name     = "${var.project_name}-${var.environment}-rg-${random_string.rg_suffix.result}"
   location = var.location
 
   tags = local.common_tags
